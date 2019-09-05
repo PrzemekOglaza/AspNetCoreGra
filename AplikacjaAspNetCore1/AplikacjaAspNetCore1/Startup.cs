d476ea6d399cc5b95ac7f5f90b551f717d563313
Przemek O <przemyslaw.oglaza@outlook.com>
2019-09-05 11:30:58 +0200

    Dodanie interfeisu Api

--- AplikacjaAspNetCore1/AplikacjaAspNetCore1/Startup.cs
+++ AplikacjaAspNetCore1/AplikacjaAspNetCore1/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using AplikacjaAspNetCore1.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AplikacjaAspNetCore1
 {
@@ -38,6 +39,11 @@ namespace AplikacjaAspNetCore1
 
             services.AddDbContext<AppDbContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("AppDbContext")));
+            // dodaje kod ze szkolenia
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +63,16 @@ namespace AplikacjaAspNetCore1
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
